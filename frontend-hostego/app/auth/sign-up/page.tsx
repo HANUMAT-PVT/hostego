367144fd6bc3e2e3e1b6c767a0861280422dab9d
HANUMAT-PVT <hanumat.sharan07@gmail.com>
2025-02-18 03:45:58 +0530

    added-auth-signup-otp-page

--- frontend-hostego/app/auth/sign-up/page.tsx
+++ frontend-hostego/app/auth/sign-up/page.tsx
@@ -4,21 +4,21 @@ import HostegoButton from "../../components/HostegoButton";
 
 const Page = () => {
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 text-white">
-      <div className="flex flex-col justify-between bg-white h-[80vh] max-w-[400px] absolute bottom-0 px-4 py-6 rounded-t-2xl">
-        <div className="flex flex-col gap-6 w-full">
+    <div className="relative min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 text-white flex items-center justify-center">
+      <div className="flex flex-col absolute justify-between bg-white h-[80vh] bottom-0 max-w-[400px] px-6 py-6 rounded-t-2xl">
+        <div className="flex flex-col gap-6 ">
           <p className="text-black font-semibold text-xl">Enter your number</p>
-          <div className="relative w-full max-w-sm">
+          <div className="relative ">
             <label className="absolute text-[#655df0] font-semibold -top-3 left-3 bg-white px-1 text-sm">
               Mobile Number
             </label>
-            <div className="flex font-semibold items-center border border-[#655df0] w-full rounded-xl px-4 py-3">
+            <div className=" flex font-semibold items-center border border-[#655df0] max-w-[400px] rounded-xl px-4 py-3 w-full">
               <span className="text-gray-700">+91</span>
               <span className="text-gray-300 ml-2">|</span>
               <input
                 type="number"
                 placeholder="Enter your number"
-                className="flex-1 ml-2 outline-none bg-transparent cursor-pointer"
+                className="ml-2 outline-none bg-transparent cursor-pointer  w-[200px]"
               />
             </div>
           </div>
