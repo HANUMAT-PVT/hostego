096110c1439fc35b39bc98f52f4d570facd48ac7
HANUMAT-PVT <hanumat.sharan07@gmail.com>
2025-02-19 04:37:58 +0530

    started implementation of home page

---
+++ frontend-hostego/app/components/BottomNavigationBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BottomNavigationBar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BottomNavigationBar
--- frontend-hostego/app/components/PhoneEmailAuth.jsx
+++ frontend-hostego/app/components/PhoneEmailAuth.jsx
@@ -27,7 +27,7 @@ const PhoneEmailAuth = () => {
     }, []);
 
     return (
-        <div data-hide-name="true" className="pe_signin_button text-center rounded-lg bg-[#655df0] text-white text-center font-bold text-lg hover:bg-[#655df0] transition duration-200 ease-in-out shadow-md"
+        <div data-hide-name="true" className="pe_signin_button flex items-center text-center rounded-lg bg-[#655df0] text-white text-center font-bold text-lg hover:bg-[#655df0] transition duration-200 ease-in-out shadow-md"
             data-client-id="13074759073192539427">
         </div>
     );
