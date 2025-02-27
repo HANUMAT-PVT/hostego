b9d977d904b4b2bdde67ee733bab999fda7279dc
HANUMAT-PVT <hanumat.sharan07@gmail.com>
2025-02-27 13:28:15 +0530

    updated

--- backend-hostego/routes/orderRoutes.go
+++ backend-hostego/routes/orderRoutes.go
@@ -9,6 +9,7 @@ import (
 func OrderRoutes(app *fiber.App) {
 	orderRoutes := app.Group("/api/order")
 	orderRoutes.Get("/", controllers.FetchAllUserOrders)
+	orderRoutes.Post("/", controllers.FetchAllUserOrders)
 	orderRoutes.Get("/:id", controllers.FetchOrderById)
 	orderRoutes.Patch("/:id", controllers.UpdateOrderById)
 	orderRoutes.Post("/mark-order-delivered", controllers.MarkOrderAsDelivered)
