273405f138e36efa5c1bd15d34e1a80ea46a8f3d
HANUMAT-PVT <hanumat.sharan07@gmail.com>
2025-03-04 23:55:22 +0530

    updated the backend

---
+++ backend-hostego/routes/roleRoutes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"backend-hostego/controllers"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func RoleRoutes(app *fiber.App) {
+	roleRoutes := app.Group("/api/roles")
+	roleRoutes.Get("/", controllers.FetchUserRoles)
+
+}
