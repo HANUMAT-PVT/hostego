ce8f592da84f108194734ff78d65ea8e212a756c
HANUMAT-PVT <hanumat.sharan07@gmail.com>
2025-03-06 04:13:13 +0530

    updatd backend user-roles ui

--- backend-hostego/controllers/userRolesController.go
+++ backend-hostego/controllers/userRolesController.go
@@ -16,11 +16,24 @@ func CreateUserRole(c fiber.Ctx) error {
 	if user_id == "" {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "User not found"})
 	}
+	var user models.UserRole
+	if err := database.DB.Where("user_id = ? AND role_id = ?", user_id, 1).First(&user).Error; err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
 	userRole := new(models.UserRole)
+
 	if err := c.Bind().JSON(userRole); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
 
+	// add check if there is 1 role id coming abort if not
+	if userRole.RoleId == 1 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Only 1 super admin role id is allowed"})
+	}
+	if user.RoleId == 2 && userRole.RoleId == 2 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "User Admin can't assign to User Admin"})
+	}
 	if err := database.DB.Create(&userRole).Error; err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
@@ -52,6 +65,14 @@ func DeleteUserRole(c fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "User not found"})
 	}
 	userRoleId := c.Params("id")
+	var userRole models.UserRole
+	if err := database.DB.Where("user_role_id = ?", userRoleId).First(&userRole).Error; err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+	if userRole.RoleId == 1 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Super Admin can't be deleted"})
+	}
+
 	if err := database.DB.Where("user_role_id = ?", userRoleId).Delete(&models.UserRole{}).Error; err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
