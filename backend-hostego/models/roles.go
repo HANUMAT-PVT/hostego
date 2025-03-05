82600e22ce76a117f351aaa60c32a7510d71723f
HANUMAT-PVT <hanumat.sharan07@gmail.com>
2025-03-06 01:20:11 +0530

    updathing

--- backend-hostego/models/roles.go
+++ backend-hostego/models/roles.go
@@ -1,14 +1,6 @@
 package models
 
-import (
-	
-)
-
 type Role struct {
-	RoleId   int `gorm:"type:int;primaryKey;not null;unique;autoIncrement;" json:"role_id"`
+	RoleId   int    `gorm:"type:int;primaryKey;not null;unique;autoIncrement;" json:"role_id"`
 	RoleName string `gorm:"type:varchar(25);" json:"role_name"`
-
 }
-
-
-
