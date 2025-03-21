c05a2af1d6b85ba564af31a86e4737bf14c77957
HANUMAT-PVT <hanumat.sharan07@gmail.com>
2025-03-21 23:15:47 +0530

    updated

--- backend-hostego/controllers/orderController.go
+++ backend-hostego/controllers/orderController.go
@@ -447,9 +447,8 @@ func FetchAllOrderItemsAccordingToProducts(c fiber.Ctx) error {
 			COUNT(DISTINCT CASE WHEN orders.created_at >= NOW() - INTERVAL '30 day' THEN order_items.order_id END) as last_month_orders
 		`).
 		Table("products").
-		Joins("LEFT JOIN order_items ON products.product_id::uuid = order_items.product_id::uuid").
-Joins("LEFT JOIN orders ON orders.order_id = order_items.order_id")
-// 
+		Joins("LEFT JOIN order_items ON products.product_id::text = order_items.product_id::text").
+		Joins("LEFT JOIN orders ON orders.order_id = order_items.order_id")
 
 	if startDate != "" && endDate != "" {
 		startDateTime := startDate + " 00:00:00"
