239a50b9141fcee2d472005c92a636f0142d2b85
HANUMAT-PVT <hanumat.sharan07@gmail.com>
2025-03-21 22:57:04 +0530

    fixed backend contorller

--- backend-hostego/models/orderItem.go
+++ backend-hostego/models/orderItem.go
@@ -2,14 +2,12 @@ package models
 
 import (
 	"time"
-
-	"github.com/google/uuid"
 )
 
 type OrderItem struct {
 	OrderItemId int       `gorm:"type:int;primaryKey;autoIncrement;" json:"order_item_id"`
 	OrderId     string    `gorm:"type:string;not null;index" json:"order_id"`
-	ProductId   uuid.UUID `gorm:"type:uuid;not null" json:"product_id"`
+	ProductId   string    `gorm:"type:uuid;not null" json:"product_id"`
 	Quantity    int       `gorm:"type:int;not null" json:"quantity"`
 	SubTotal    float64   `gorm:"type:double precision;not null" json:"sub_total"`
 	Product     Product   `gorm:"foreignKey:ProductId;references:ProductId" json:"product"`
