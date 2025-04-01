b0efe4e353f8cf63d82626453b85a62051e53860
HANUMAT-PVT <hanumat.sharan07@gmail.com>
2025-04-01 15:04:55 +0530

    implemented stop instagram warning message

--- frontend-hostego/app/components/ProductCard.jsx
+++ frontend-hostego/app/components/ProductCard.jsx
@@ -39,8 +39,8 @@ const ProductCard = ({
         <div key={myKey} className='bg-white rounded-xl border border-gray-100 relative'>
             {/* Shop Closed Overlay */}
             {isShopClosed && (
-                <div className="absolute top-3 right-3">
-                    <div className="bg-red-500 text-white text-xs px-2 py-0.5 rounded-md z-10">
+                <div className="absolute top-3 right-3 z-[20]">
+                    <div className="bg-red-500 text-white text-xs px-2 py-0.5 rounded-md z-[20]">
                         Shop  Closed
                     </div>
                 </div>
