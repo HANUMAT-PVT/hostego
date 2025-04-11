ecd86172a6b878f249e34b63dbc813e4e95525a1
HANUMAT-PVT <hanumat.sharan07@gmail.com>
2025-04-11 12:12:34 +0530

    updated the frontend ship and refund policiy

---
+++ frontend-hostego/app/refund-and-cancellation/page.jsx
@@ -0,0 +1,65 @@
+
+
+export default function RefundPolicy() {
+    return (
+        <>
+
+            <title>Cancellation & Refund Policy | HOSTEGO</title>
+
+            <main className="max-w-4xl mx-auto px-4 py-10 text-gray-800">
+                <h1 className="text-3xl font-bold mb-6 border-b pb-2">
+                    Cancellation & Refund Policy
+                </h1>
+
+                <p className="text-sm text-gray-500 mb-4">
+                    Last updated on 11-04-2025 12:00:28
+                </p>
+
+                <p className="mb-4">
+                    <strong>(HOSTEGO) </strong> HANUMAT SHARAN  believes in helping its customers as far as
+                    possible and has therefore a liberal cancellation policy. Under this policy:
+                </p>
+
+                <ul className="list-disc list-inside space-y-3">
+                    <li>
+                        Cancellations will be considered only if the request is made
+                        immediately after placing the order. However, the cancellation request may
+                        not be entertained if the orders have been communicated to the vendors/merchants
+                        and they have initiated the shipping process.
+                    </li>
+                    <li>
+                        <strong>(HOSTEGO) </strong> HANUMAT SHARAN does not accept cancellation requests for
+                        perishable items like flowers, eatables, etc. However, a refund/replacement can
+                        be made if the customer establishes that the quality of the delivered product is
+                        not good.
+                    </li>
+                    <ul className="list-disc pl-5 space-y-2">
+                        <li>Orders can be cancelled before the delivery partner picks up the order</li>
+                        <li>Refunds will be processed according to our refund policy</li>
+                        <li>Refunds will be processed according to our refund policy and will be added to the user's wallet.</li>
+                        <li>Repeated cancellations may result in service restrictions</li>
+                    </ul>
+                    <li>
+                        In case of receipt of damaged or defective items, please report the same to our
+                        Customer Service team. The request will be entertained once the merchant has
+                        checked and confirmed the same at their end. This should be reported within{" "}
+                        <strong>the same day</strong> of receipt of the products.
+                    </li>
+                    <li>
+                        If you feel that the product received is not as shown on the site or as per your
+                        expectations, you must bring it to the notice of our customer service within{" "}
+                        <strong>the same day</strong> of receiving the product. The Customer Service Team,
+                        after reviewing your complaint, will take an appropriate decision.
+                    </li>
+                    <li>
+                        For products with a manufacturer warranty, please refer the issue directly to them.
+                    </li>
+                    <li>
+                        In case of any refunds approved by <strong>(HOSTEGO) </strong>HANUMAT SHARAN it will take{" "}
+                        <strong>3–5 business days</strong> for the refund to be processed to the end customer.
+                    </li>
+                </ul>
+            </main>
+        </>
+    );
+}
