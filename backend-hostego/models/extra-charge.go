b3e46927b1b212f0fb1ce0bcc2b820d549ebad0f
HANUMAT-PVT <hanumat.sharan07@gmail.com>
2025-04-12 15:39:11 +0530

    updated the rain extra charge

--- backend-hostego/models/extra-charge.go
+++ backend-hostego/models/extra-charge.go
@@ -1,5 +1,12 @@
 package models
 
+import "time"
+
 type ExtrCharge struct {
-	AddressID int `gorm:"type:int;primaryKey;not null;autoIncrement:true" json:"address_id"`
+	Id         int       `gorm:"type:int;primaryKey;not null;autoIncrement:true" json:"id"`
+	ChargeType string    `gorm:"type:string;" json:"charge_type"`
+	Amount     float64   `gorm:"type:float;" json:"charge_amount"`
+	CreatedAt  time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt  time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	Status     string    `gorm:"type:string;" json:"status_type"`
 }
