7788d88d90cc6678ad95306827304a6aab63ccba
HANUMAT-PVT <hanumat.sharan07@gmail.com>
2025-04-25 22:56:01 +0530

    updated the backend razorpay web hook

--- backend-hostego/routes/paymentRoutes.go
+++ backend-hostego/routes/paymentRoutes.go
@@ -33,5 +33,5 @@ func PaymentRoutes(app *fiber.App) {
 func PaymentWebhookRoutes(app * fiber.App){
 	webhookRoutes := app.Group("/api/razorpay/webhook")
 	// https://backend.hostego.in/api/razorpay/webhook/payment/verify
-	webhookRoutes.Post("/payment/verify",controllers.RazorpayWebhookHandler);
+	webhookRoutes.Post("/payment/verify-order",controllers.RazorpayWebhookHandler);
 }
