aa324ce7bb81552f570ade70da2c6c67097408ce
Hanumat-PVT <hanumat.sharan07@gmail.com>
2025-07-31 00:03:43 +0530

    updated the backend

--- backend-hostego/database/database.go
+++ backend-hostego/database/database.go
@@ -17,7 +17,7 @@ func ConnectDataBase() {
 	//postgres for dev
 	// dsn := "host=localhost user=postgres password=hostego_hanumat dbname=hostego_db_dev port=5432 sslmode=disable"
 	// hostego_user_dev for prod
-	dsn := "host=localhost user=postgres password=hostego_hanumat dbname=hostego_db_dev port=5432 sslmode=disable"
+	dsn := "host=localhost user=hostego_user_dev password=hostego_hanumat dbname=hostego_db_dev port=5432 sslmode=disable"
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		// DisableForeignKeyConstraintWhenMigrating: true,
