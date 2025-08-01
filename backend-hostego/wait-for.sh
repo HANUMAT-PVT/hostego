1265c535c8278f3415fad292bd72e619ed069c7b
Hanumat-PVT <hanumat.sharan07@gmail.com>
2025-08-01 18:31:07 +0530

    updated

--- backend-hostego/wait-for.sh
+++ backend-hostego/wait-for.sh
@@ -6,9 +6,9 @@ CMD="$@"
 
 echo "⏳ Waiting for $HOST..."
 
-while ! nc -z $(echo $HOST | cut -d: -f1) $(echo $HOST | cut -d: -f2); do
-  sleep 1
-done
+# while ! nc -z $(echo $HOST | cut -d: -f1) $(echo $HOST | cut -d: -f2); do
+#   sleep 1
+# done
 
 echo "✅ $HOST is available, running: $CMD"
 exec $CMD
