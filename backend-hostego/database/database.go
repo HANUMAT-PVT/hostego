f025e58702fbb849f33da043d6beb449e8e5fafd
Hanumat-PVT <hanumat.sharan07@gmail.com>
2025-08-19 17:51:16 +0530

    updated

--- backend-hostego/database/database.go
+++ backend-hostego/database/database.go
@@ -16,9 +16,9 @@ var DB *gorm.DB
 
 func ConnectDataBase() {
 	//postgres for dev
-	dsn := "host=localhost user=postgres password=hostego_hanumat dbname=hostego_db_dev port=5432 sslmode=disable"
+	// dsn := "host=localhost user=postgres password=hostego_hanumat dbname=hostego_db_dev port=5432 sslmode=disable"
 	// hostego_user_dev for prodai
-	// dsn := "host=localhost user=hostego_user_dev password=hostego_hanumat dbname=hostego_db_dev port=5432 sslmode=disable"
+	dsn := "host=localhost user=hostego_user_dev password=hostego_hanumat dbname=hostego_db_dev port=5432 sslmode=disable"
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 
